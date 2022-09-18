7bc58f40c27936b1120b9460c5f23d8e36a87a67
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2022-09-18 21:43:25 +0700

    Create Order Page

--- src/pages/order/index.js
+++ src/pages/order/index.js
@@ -1,22 +1,78 @@
+import { getOrders } from "../../services/fake-apis/index";
+import queryString from "query-string";
+
 Page({
   data: {
-    categories: [
+    isLoading: false,
+    activeTab: 1,
+    orderTabs: [
       {
         id: 1,
-        name: "All",
+        title: "All",
       },
       {
         id: 2,
-        name: "Chờ thanh toán",
+        title: "Chờ thanh toán",
       },
       {
         id: 3,
-        name: "Đang lấy",
+        title: "Đang tiến hành",
       },
       {
         id: 4,
-        name: "Đang giao",
+        title: "Đang vận chuyển",
       },
     ],
+    orders: [],
+  },
+
+  onTabClick({ index, tabsName }) {
+    this.setData({
+      [tabsName]: index,
+    });
+  },
+  onChangeTab({ index, tabsName }) {
+    if (!this.data.isLoading) {
+      this.setData({
+        [tabsName]: index,
+      });
+    }
+    this.loadData();
+  },
+
+  onMoveOrderDetail() {
+    my.navigateTo({ url: "pages/order-detail/index" });
+  },
+
+  async loadData() {
+    this.setData({
+      isLoading: true,
+    });
+
+    try {
+      const orders = await getOrders();
+
+      this.setData({
+        orders,
+        isLoading: false,
+      });
+    } catch (error) {
+      this.setData({
+        isLoading: false,
+      });
+    }
+  },
+
+  // Life cycle
+  onLoad(query) {
+    const { tab } = queryString.parse(query);
+    this.setData({
+      activeTab: parseInt(tab) + 1,
+    });
+    console.log(query);
+  },
+
+  onReady() {
+    this.loadData();
   },
 });
--- src/utils/common.sjs
+++ src/utils/common.sjs
@@ -8,3 +8,25 @@ export const numberFormatter = (number, suffix = "") => {
   if (!isNotEmpty(number)) return "";
   return parseInt(number).toLocaleString("vi-VN") + suffix;
 };
+
+export const statusFormatter = (status) => {
+  let statusFormatter = status;
+  switch (status) {
+    case "delievered":
+      statusFormatter = "Đã giao";
+      break;
+    case "shipping":
+      statusFormatter = "Đang vận chuyển";
+      break;
+    case "processing":
+      statusFormatter = "Đang tiến hành";
+      break;
+    case "waiting":
+      statusFormatter = "Đang chờ giao";
+      break;
+    case "canceled":
+      statusFormatter = "Đã hủy";
+      break;
+  }
+  return statusFormatter;
+};
